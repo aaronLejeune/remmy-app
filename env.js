cc1475527cd58a37d2099fdef3573fda2ad84a81
aaron lejeune <aarontrol@gmail.com>
2020-08-17 15:52:26 +0200

    small bugfix

--- env.js
+++ env.js
@@ -4,6 +4,6 @@ module.exports = {
   // Configure from Google API
   YOUR_CLIENT_ID: '623650943997-cpp3mnseuok9rq55jfq0cb5j93fqcjfi.apps.googleusercontent.com',
   YOUR_CLIENT_SECRET: 'irJFP7x9zuCeUGwNs-bsfz6h',
-  YOUR_REDIRECT_URL: 'http://104.248.22.138:4000/redirect',
+  YOUR_REDIRECT_URL: 'https://photos.aaronlejeune.be/redirect',
   YOUR_APIKEY: 'AIzaSyAqmXIZgjcaKQ8SG4KbciVrgxn124P21UA',
 };
